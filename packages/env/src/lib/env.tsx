f771b3bbf963925d1a3b74719bd572c39ec21a73
Othi <mnpq.raven@gmail.com>
2025-04-25 09:37:12 +0700

    feat(env): scaffold

---
+++ packages/env/src/lib/env.tsx
@@ -0,0 +1,11 @@
+import styles from './env.module.css';
+
+export function Env() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Env!</h1>
+    </div>
+  );
+}
+
+export default Env;
