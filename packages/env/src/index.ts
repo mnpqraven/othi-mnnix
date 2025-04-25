b78be15004ad28174c24d4109d2c7ab3f014046c
Othi <mnpq.raven@gmail.com>
2025-04-25 11:11:57 +0700

    feat(env): clean + lint, unchecked

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,1 +1,12 @@
-export * from './lib/env';
+import { createEnv } from "@t3-oss/env-nextjs";
+import { authEnv } from "./auth";
+import { storageEnv } from "./storage";
+import { hostsEnv } from "./hosts";
+import { debugEnv } from "./debug";
+
+export const env = createEnv({
+  server: {},
+  client: {},
+  experimental__runtimeEnv: process.env,
+  extends: [authEnv, storageEnv, hostsEnv, debugEnv],
+});
