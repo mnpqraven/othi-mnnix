d7e496aea66ff74737517bf64c4d7a9324e63555
Othi <mnpq.raven@gmail.com>
2025-04-25 11:05:16 +0700

    clear old routes

--- packages/protocol/src/trpc/index.ts
+++ packages/protocol/src/trpc/index.ts
@@ -1,16 +1,12 @@
 import "server-only";
 
 import { createCallerFactory, router } from "./trpc";
-import { tableRouter } from "./routers/table";
-import { honkaiRouter } from "./routers/honkai";
 import { othiRouter } from "./routers/othi";
 import { blogUtilsRouter } from "./routers/utils/blog";
 import type { RouterInputs, RouterOutputs } from "./react/client";
 import { blogRouter } from "./routers/blog";
 
 export const appRouter = router({
-  honkai: honkaiRouter,
-  table: tableRouter,
   utils: {
     blog: blogUtilsRouter,
   },
