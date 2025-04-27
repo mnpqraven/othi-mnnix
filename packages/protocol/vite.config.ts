14f946067c006eab6b1013b34c0236bba0db8e17
Othi <mnpq.raven@gmail.com>
2025-04-27 20:53:20 +0700

    Biome migrate (#5)

--- packages/protocol/vite.config.ts
+++ packages/protocol/vite.config.ts
@@ -42,15 +42,4 @@ export default defineConfig(() => ({
     reportCompressedSize: true,
     commonjsOptions: { transformMixedEsModules: true },
   },
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: './test-output/vitest/coverage',
-      provider: 'v8' as const,
-    },
-  },
 }));
